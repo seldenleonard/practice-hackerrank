f823426042b8a250721225f260324c554d362e20
seldenleonard <selden222@gmail.com>
2020-12-11 22:31:54 -0500

    adds working version of birthday cake candles algorithm

--- birthday-cake-candles-problem.rb
+++ birthday-cake-candles-problem.rb
@@ -3,12 +3,44 @@
 # STEPS
 # create a method that takes in an array
 # set the variable "tallest_candle" equal to 0 (because constraints do not allow negative numbers - so no issue there)
-# make an index and loop to iterate through each array value, comparing each value to the one following it, then set "tallest_candle" to whichever is larger (or just use .sort method and set the "tallest_candle" variable to the value of array.last - then you can start at the end and iterate backwards, counting how many array values these are equal to "tallest candle", and end the loop/iterating when the array value is no longer equal to "tallest_candle")
+  # ------- (or just set it to index 0 of the array (the first value of the array))
+# make an index and loop to iterate through each array value, comparing each value to the one following it, then set "tallest_candle" to whichever is larger
+  # ------- (or just use .sort method and set the "tallest_candle" variable to the value of array.last - then you can start at the end and iterate backwards, counting how many array values these are equal to "tallest candle", and end the loop/iterating when the array value is no longer equal to "tallest_candle")
 # once the entire array has been evaluated, reset the index to 0, and iterate through the array a 2nd time, adding 1 to a variable that is keeping count of how many instances of the "tallest_candle" value exist in the array.
-# print "tallest_candle" value
+# output the count of "tallest_candle" occurences in the array
 
 # Idea to make code my DRY - instead of looping through a 2nd time, count during the first loop, and add a conditional where if the "tallest_candle" value changes, reset count to 1.
 
-def birthdayCakeCandles
-  
+# def birthdayCakeCandles(candles)
+#   i = 0
+#   # tallest_candle = candles[0]
+#   tallest_candle_count = 0
+#   while i < candles.length - 1
+#     if candles[i] > candles[i + 1]
+#       tallest_candle = candles[i]
+#       tallest_candle_count += 1
+#     elsif
+#       tallest_candle = candles[i + 1]
+#     end
+#     i += 1
+#   end
+#   p tallest_candle, tallest_candle_count
+# end
+
+def birthdayCakeCandles(candles)
+  i = 1
+  tallest_candle = candles[0]
+  tallest_candle_count = 0
+  while i < candles.length
+    if tallest_candle < candles[i]
+      tallest_candle = candles[i]
+      # tallest_candle_count = 1
+    elsif tallest_candle = candles[i] 
+      tallest_candle_count += 1
+    end
+    i += 1
+  end
+  p tallest_candle, tallest_candle_count
 end
+
+birthdayCakeCandles([3, 2, 1, 3])
