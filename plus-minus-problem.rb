fed0d21d54471746de4d7bd57dc37dbefddf9982
seldenleonard <selden222@gmail.com>
2020-12-13 22:53:57 -0500

    updates whiteboarding steps to solve plus minus problem

--- plus-minus-problem.rb
+++ plus-minus-problem.rb
@@ -3,16 +3,19 @@
 
 # STEPS
 # Create method that takes in an array
-# Set variable called "denominator" to length of the array
-# Set variable called "negative_numerator"
-# Set variable called "positive_numerator"
-# Set variable called "zero_numerator"
+# Set a variable called "denominator" to length of the array
+# Set a variable called "negative_numerator" equal to zero
+# Set a variable called "positive_numerator" equal to zero
+# Set a variable called "zero_numerator" equal to zero
 # Create an index
 # Loop through the array and at each value evaluate whether it is positive, negative, or zero (use "if" "elsif" "else"). Add 1 to the whichever of the three numerator variables the array value falls under.
 # return each of the three numerator variable each divided by the denominator variable. (could also not even have a denominator variable at all and just use array.length)
 
 def plus_minus(arr)
   denominator = arr.length
+  negative_numerator = 0
+  positive_numerator = 0
+  zero_numerator = 0
   i = 0
 end
 
