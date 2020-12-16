fad8eab6e84a09b9f43a4c10b5d3575faa950b30
seldenleonard <selden222@gmail.com>
2020-12-15 23:59:27 -0500

    writes out all the steps to complete the diagional difference problem

--- diagonal-difference-problem.rb
+++ diagonal-difference-problem.rb
@@ -1,2 +1,8 @@
 # Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 
+# STEPS
+# Create a method that takes in 3 arrays
+# Use an index that ends at the length or any single array (because all arrays are same length because it is a square matrix)
+# To get the sum of the first diagonal, set index to zero, then add up the value of each array at the index value, and have the index increase by one for each array it evaluates.
+# To get sum of the second diagonal, set index to length of any one of the input arrays, then add up the value of each array at the index value, and have the index decrease by one each for array it evaluates.
+# Store the two sums in variables, then subtract the smaller value from the larger value
