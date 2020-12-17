781b6f7f7f5329c911012066861a17335700cf54
seldenleonard <selden222@gmail.com>
2020-12-16 23:43:20 -0500

    solves apples-and-oranges-problem

--- apples-and-oranges-problem.rb
+++ apples-and-oranges-problem.rb
@@ -4,3 +4,27 @@
 # map through oranges and add position of tree to orange distance
 # map through the new apple array and if the new number is between the range of the house, add a number to the counter
 # map through the new orange array and if the new number is between the range of the house, add a number to the counter
+
+def countApplesAndOranges(s, t, a, b, apples, oranges)
+  apples_on_house = 0
+  oranges_on_house = 0
+  apples.map! do |apple|
+    apple + a
+  end
+  oranges.map! do |orange|
+    orange + b
+  end
+  apples.each do |apple|
+    if apple >= s && apple <= t
+      apples_on_house += 1
+    end
+  end
+  oranges.each do |orange|
+    if orange >= s && orange <= t
+      oranges_on_house += 1
+    end
+  end
+  p apples_on_house
+  p oranges_on_house
+end
+countApplesAndOranges(7, 11, 5, 5, [-2, 2, 1], [5, -6])
