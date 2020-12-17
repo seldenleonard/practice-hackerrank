9f3f74841bce48c7b0b2458e83fbaa200950fac1
seldenleonard <selden222@gmail.com>
2020-12-16 23:34:25 -0500

    creates steps to solve apples-and-oranges-problem

---
+++ apples-and-oranges-problem.rb
@@ -0,0 +1,7 @@
+# STEPS
+
+# loop through and print out each number in the array
+# conditional statement to determine whether the grade is failing or passing (over or below 38)
+# conditional statement that determines whether the number is divisible by 5 or not
+# conditional statement that if you add 1 to the grade and modulo is 0, add 1 to the number to round it
+# conditional statement that says if you add 2 to the grade and modulo is 0, add 2 to the number to round it
