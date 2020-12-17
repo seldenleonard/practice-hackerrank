e547eff1f73c0a26a29afae73f2ef13693b70fbe
seldenleonard <selden222@gmail.com>
2020-12-16 23:48:28 -0500

    adds steps for resolving grading-students-problem.rb file

---
+++ grading-students-problem.rb
@@ -0,0 +1,17 @@
+# # HackerLand University has the following grading policy:
+
+# Every student receives a  in the inclusive range from  to .
+# Any  less than  is a failing grade.
+# Sam is a professor at the university and likes to round each student's  according to these rules:
+
+# If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
+# If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+
+
+# STEPS
+
+# loop through and print out each number in the array
+# conditional statement to determine whether the grade is failing or passing (over or below 38)
+# conditional statement that determines whether the number is divisible by 5 or not
+# conditional statement that if you add 1 to the grade and modulo is 0, add 1 to the number to round it
+# conditional statement that says if you add 2 to the grade and modulo is 0, add 2 to the number to round it
