443e2cbc13359433f89e06fa200b7b68663aa71c
seldenleonard <selden222@gmail.com>
2020-12-17 23:45:44 -0500

    creates the-birthday-bar-problem.rb file

--- diagonal-difference-problem.rb
+++ diagonal-difference-problem.rb
@@ -2,7 +2,15 @@
 
 # STEPS
 # Create a method that takes in 3 arrays
-# Use an index that ends at the length or any single array (because all arrays are same length because it is a square matrix)
+# Use an index that ends at the length of any single array (because all arrays are same length because it is a square matrix)
 # To get the sum of the first diagonal, set index to zero, then add up the value of each array at the index value, and have the index increase by one for each array it evaluates.
 # To get sum of the second diagonal, set index to length of any one of the input arrays, then add up the value of each array at the index value, and have the index decrease by one each for array it evaluates.
 # Store the two sums in variables, then subtract the smaller value from the larger value
+
+def diagonal_difference(arr1, arr2, arr3)
+  i = 0
+  array_of_arrays = 
+
+end
+
+diagonal_difference([11, 2, 4], [4, 5, 6], [10, 8, -12])
---
+++ the-birthday-bar-problem.rb
@@ -0,0 +1,19 @@
+# https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+# Given a chocolate bar, two children, Lily and Ron, are determining how to share it. Each of the squares has an integer on it.
+
+# Lily decides to share a contiguous segment of the bar selected such that:
+
+# The length of the segment matches Ron's birth month, and,
+# The sum of the integers on the squares is equal to his birth day.
+# You must determine how many ways she can divide the chocolate.
+
+# Function Description
+
+# Complete the birthday function in the editor below. It should return an integer denoting the number of ways Lily can divide the chocolate bar.
+
+# birthday has the following parameter(s):
+
+# s: an array of integers, the numbers on each of the squares of chocolate
+# d: an integer, Ron's birth day
+# m: an integer, Ron's birth month
