e0612cf5fdcc77c52eb4bcf2f57f070cc04c58e9
seldenleonard <selden222@gmail.com>
2020-12-20 23:58:24 -0500

    dry's up code in compare the triplets solution

--- compare-the-triplets.rb
+++ compare-the-triplets.rb
@@ -13,7 +13,6 @@
 
 # STEPS
 
-
 def compareTriplets(a, b)
   a_score = 0
   b_score = 0
@@ -27,6 +26,6 @@ def compareTriplets(a, b)
       index += 1
   end
   score_array = [a_score, b_score]
-  score_array
+  # score_array
 end
 p compareTriplets([5, 6, 7], [3, 6, 10])
