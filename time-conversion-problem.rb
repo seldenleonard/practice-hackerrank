1ea5e448939cb1b6b28c40c00a0f13bbe40622f2
seldenleonard <selden222@gmail.com>
2020-12-21 02:02:12 -0500

    adds instructions for time conversion problem

--- time-conversion-problem.rb
+++ time-conversion-problem.rb
@@ -3,3 +3,7 @@
 # Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
 # - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
 
+# STEPS
+# Create method that takes in a single string
+# Use "if" statements and conditionals to change values at specific values within the string, by iterating through the string
+# For exmaple: if str[0] and str[1] are "1" and "2", AND str[9] is "A" make str[0] and str[1] in the output "0" and "0"
