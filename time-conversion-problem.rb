ea1a2b67101675bb1f44f0d876fb5f44e2fbcddb
seldenleonard <selden222@gmail.com>
2020-12-24 04:21:40 -0500

    deletes commented-out first attempt to solve time-conversion-problem.rb

--- time-conversion-problem.rb
+++ time-conversion-problem.rb
@@ -4,24 +4,6 @@
 # - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
 
 # STEPS
-# Create method that takes in a single string
-# Use "if" statements and conditionals to change values at specific values within the string, by iterating through the string
-# For exmaple: if time[0] and time[1] are "1" and "2", AND time[9] is "A" make time[0] and time[1] in the output "0" and "0"
-
-# def time_conversion(time)
-#   military_time = "11:01:00"
-#   if time[0] == "1" && time[1] == "2" && time[9] == "A"
-#     military_time[0] = "0"
-#     military_time[1] = "0"
-#   end
-#   p military_time
-# end
-
-# time_conversion("07:05:45PM")
-
-
-
-# STEPS
 # AM
 #   If AM use string[-2] to pop off the last two characters in the string -- so its military AM
 #     - Inside ^this conditional, have another if conditional that the 12 at the beggining of the string to 00
