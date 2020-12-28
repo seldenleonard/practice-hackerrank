921fb8b2b1cc5df81827130f182d5352edff57d5
seldenleonard <selden222@gmail.com>
2020-12-27 22:12:59 -0500

    adds foundation neccessary to solve pangram problem

--- pangram-problem.rb
+++ pangram-problem.rb
@@ -1,2 +1,8 @@
 # A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
 
+# STEPS
+# Create a methdo that takes in a string and searches for every letter of the alphabet it in it
+
+def pangram?(str)
+  
+end
