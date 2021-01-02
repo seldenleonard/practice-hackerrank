74b4a9b5b1ded88be9ee93a5ed04964213e489fd
seldenleonard <selden222@gmail.com>
2021-01-01 20:42:46 -0500

    adds steps to migratory birds problem

---
+++ migratory-birds-problem.rb
@@ -0,0 +1,8 @@
+# STEPS
+# Loop through array and print integers and set up counters
+# Write conditionals to add to counters for each bird id
+# Write conditionals to comparing most seen bird to each other bird id
+  # If most seen is greater, do nothing
+  # If most seen is smaller, set bird id being compared to most seen variable
+  # If most seen is equal, do nothing
+# print index
