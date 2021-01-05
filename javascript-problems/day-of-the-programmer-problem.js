7d3ba9fdcd84f0427c24a4c30fec8137e59663bc
seldenleonard <selden222@gmail.com>
2021-01-05 15:36:15 -0500

    solves day of the programmer problem

--- javascript-problems/day-of-the-programmer-problem.js
+++ javascript-problems/day-of-the-programmer-problem.js
@@ -8,4 +8,27 @@
 // if year is in gregarian calendar, figure out whether it’s a leap year (divisible by 4, but not 100 and divisible by 400) or not
   // if it is a leap year, print 12.09.year otherwise print 13.09.year
 
-  
+function dayOfProgrammer(year) {
+  if (year < 1918) {
+    // Julien
+    if (year % 4 === 0) {
+      return `12.09.${year}`;
+    }
+    else {
+      return `13.09.${year}`;
+    }
+  }
+  if (year > 1918) {
+    // Gregarian
+    if (year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
+      return `12.09.${year}`;
+    }
+    else {
+      return `13.09.${year}`;
+    }
+  }
+  if (year === 1918) {
+    return `26.09.${year}`;
+  }
+}
+dayOfProgrammer(1918);
