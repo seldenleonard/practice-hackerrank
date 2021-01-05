76176499b9654de4fe64de2de59ce766a58d041d
seldenleonard <selden222@gmail.com>
2021-01-05 15:35:33 -0500

    Writes out steps to solve day of the programmer problem

--- javascript-problems/day-of-the-programmer-problem.js
+++ javascript-problems/day-of-the-programmer-problem.js
@@ -1,1 +1,11 @@
 // https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+
+// STEPS
+// Print out year
+// determine whether year is in julien or gregarian calendar
+// if year is in julien calendar, figure out whether it’s a leap year (divisible by 4) or not
+  // if it is a leap year, print 12.09.year otherwise print 13.09.year
+// if year is in gregarian calendar, figure out whether it’s a leap year (divisible by 4, but not 100 and divisible by 400) or not
+  // if it is a leap year, print 12.09.year otherwise print 13.09.year
+
+  
