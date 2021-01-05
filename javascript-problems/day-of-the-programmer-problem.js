671c02f4f345f3a6810ed94b78d39a0c311c036c
seldenleonard <selden222@gmail.com>
2021-01-05 15:37:16 -0500

    fixes js formatting in day of the programmer solution

--- javascript-problems/day-of-the-programmer-problem.js
+++ javascript-problems/day-of-the-programmer-problem.js
@@ -2,19 +2,18 @@
 
 // STEPS
 // Print out year
-// determine whether year is in julien or gregarian calendar
-// if year is in julien calendar, figure out whether it’s a leap year (divisible by 4) or not
+// determine whether year is in julian or gregarian calendar
+// if year is in julian calendar, figure out whether it’s a leap year (divisible by 4) or not
   // if it is a leap year, print 12.09.year otherwise print 13.09.year
 // if year is in gregarian calendar, figure out whether it’s a leap year (divisible by 4, but not 100 and divisible by 400) or not
   // if it is a leap year, print 12.09.year otherwise print 13.09.year
 
 function dayOfProgrammer(year) {
   if (year < 1918) {
-    // Julien
+    // Julian
     if (year % 4 === 0) {
       return `12.09.${year}`;
-    }
-    else {
+    } else {
       return `13.09.${year}`;
     }
   }
@@ -22,8 +21,7 @@ function dayOfProgrammer(year) {
     // Gregarian
     if (year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
       return `12.09.${year}`;
-    }
-    else {
+    } else {
       return `13.09.${year}`;
     }
   }
