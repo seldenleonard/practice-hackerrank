9311f8245af1799ddbac7b9a8fe7322d8162114e
seldenleonard <selden222@gmail.com>
2021-01-06 04:38:22 -0500

    adds steps to bill division problem

--- javascript-problems/bill-division-problem.js
+++ javascript-problems/bill-division-problem.js
@@ -1,1 +1,8 @@
 // https://www.hackerrank.com/challenges/bon-appetit/problem
+
+// STEPS
+// Loop through and print each price of food in the array
+// print the price of the item anna didn’t eat
+// add all items anna did eat (minus what she didn’t eat) and print
+// if what anna did eat divided by 2 is equal to what anna contributed to the meal, print bon appetit
+// if not, subtract what anna ate divided by 2 from what she paid and print number
