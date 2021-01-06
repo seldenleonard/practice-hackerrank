45c081163af750a17cf5be336cbacb0259529998
seldenleonard <selden222@gmail.com>
2021-01-06 04:36:42 -0500

    creates bill-division-problem.js file and adds link to hackerrank problem

--- javascript-problems/day-of-the-programmer-problem.js
+++ javascript-problems/day-of-the-programmer-problem.js
@@ -4,9 +4,9 @@
 // Print out year
 // determine whether year is in julian or gregarian calendar
 // if year is in julian calendar, figure out whether it’s a leap year (divisible by 4) or not
-  // if it is a leap year, print 12.09.year otherwise print 13.09.year
+// if it is a leap year, print 12.09.year otherwise print 13.09.year
 // if year is in gregarian calendar, figure out whether it’s a leap year (divisible by 4, but not 100 and divisible by 400) or not
-  // if it is a leap year, print 12.09.year otherwise print 13.09.year
+// if it is a leap year, print 12.09.year otherwise print 13.09.year
 
 function dayOfProgrammer(year) {
   if (year < 1918) {
