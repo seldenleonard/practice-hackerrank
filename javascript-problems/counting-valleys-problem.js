05d288436dbc3f2747565f1f6534fb1c3282711f
seldenleonard <selden222@gmail.com>
2021-01-07 14:20:38 -0500

    adds steps to solve counting-valleys-problem.js

--- javascript-problems/counting-valleys-problem.js
+++ javascript-problems/counting-valleys-problem.js
@@ -1,1 +1,6 @@
 // https://www.hackerrank.com/challenges/counting-valleys/problem?h_r=internal-search
+
+// STEPS
+// Loop and print
+// set sea level variable to keep track of above or below zero
+// loop through and add or subract one based off of D or U
