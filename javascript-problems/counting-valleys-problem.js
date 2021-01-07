292862d543b8146446dc65c232edfcda77f09322
seldenleonard <selden222@gmail.com>
2021-01-07 16:20:16 -0500

    adds first attempt at solving counting-valleys-problem.rb

--- javascript-problems/counting-valleys-problem.js
+++ javascript-problems/counting-valleys-problem.js
@@ -4,3 +4,22 @@
 // Loop and print
 // set sea level variable to keep track of above or below zero
 // loop through and add or subract one based off of D or U
+
+function countingValleys(steps, path) {
+  var i = 0;
+  var seaLevel = 0;
+  var valleyCounter = 0;
+  while (i < path.length - 1) {
+    if (i === 0 && path[i] === "D" || seaLevel === 0 && path[i + 1] === "D") {
+      console.log(`valley counter ${valleyCounter += 1}`);
+    }
+    if (path[i] === "D") {
+      console.log(`sea level counter in D ${seaLevel -= 1}`);
+    } else if (path[i] === "U") {
+      console.log(`sea level counter in U ${seaLevel += 1}`);
+    }
+    i += 1;
+  }
+  return valleyCounter;
+}
+console.log(countingValleys(8, "UDDDUDUU"));
