313c89278962c12e3cdb3239392489fea57d9fe9
seldenleonard <selden222@gmail.com>
2021-01-09 19:27:07 -0500

    adds first attempt to solve drawing book problem

--- javascript-problems/drawing-book-problem.js
+++ javascript-problems/drawing-book-problem.js
@@ -7,3 +7,25 @@
 // when starting at back of book, if total number of pages is even, first subract 1 and add 1 to a page counter, then subtract 2 and add 1 to page counter until you get to the page number
 // when starting at the front of the book, add 2 to the index and add 1 to the counter until you get to the page number
 // print counter
+
+function pageCount(n, p) {
+  let counter = 0;
+  if (n / 2 <= p) {
+    //start from back
+    let startingPage = n;
+    if (n % 2 === 0) {
+      startingPage = startingPage - 1;
+      counter += 1;
+    }
+    while (startingPage >= p) {
+      startingPage -= 2;
+      counter += 1;
+    }
+    return counter;
+  } else if (n / 2 > p) {
+    //start from front
+    let startingPage = 1;
+    return "start from front";
+  }
+}
+console.log(pageCount(6, 4));
