74795d8cd7d8a04d1ae9f54f539e76bb803a7db6
seldenleonard <selden222@gmail.com>
2021-01-09 19:29:52 -0500

    solves drawing-book-problem.rb

--- javascript-problems/drawing-book-problem.js
+++ javascript-problems/drawing-book-problem.js
@@ -10,22 +10,28 @@
 
 function pageCount(n, p) {
   let counter = 0;
-  if (n / 2 <= p) {
+  let startingPage = 0;
+  if (n % 2 !== 0 && n - 1 === p || p === 1) {
+    counter = 0;
+  } else if (n / 2 < p) {
     //start from back
-    let startingPage = n;
-    if (n % 2 === 0) {
-      startingPage = startingPage - 1;
-      counter += 1;
+    if (n % 2 !== 0) {
+      startingPage = n - 1;
+    } else {
+      startingPage = n;
     }
-    while (startingPage >= p) {
+    while (startingPage > p) {
       startingPage -= 2;
       counter += 1;
     }
-    return counter;
-  } else if (n / 2 > p) {
+  } else if (n / 2 >= p) {
     //start from front
-    let startingPage = 1;
-    return "start from front";
+    startingPage = 1;
+    while (startingPage < p) {
+      startingPage += 2;
+      counter += 1;
+    }
   }
+  return counter;
 }
-console.log(pageCount(6, 4));
+console.log(pageCount(6, 3));
