de8dd4e4aacbb26372de46d0387882abc583108d
seldenleonard <selden222@gmail.com>
2021-01-14 22:47:36 -0500

    adds links to birthday-cake-candles.rb and compare-the-triplets.rb files hackerrank problems

--- ruby-problems/birthday-cake-candles-problem.rb
+++ ruby-problems/birthday-cake-candles-problem.rb
@@ -1,3 +1,5 @@
+# https://www.hackerrank.com/challenges/birthday-cake-candles/problem?h_r=internal-search
+
 # You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
 
 # STEPS
