40ffd8a767ce3c8bf70c9b4791374e01de12cdf6
seldenleonard <selden222@gmail.com>
2021-01-17 21:14:32 -0500

    adds grading students link to hackerrank probelm

--- ruby-problems/grading-students-problem.rb
+++ ruby-problems/grading-students-problem.rb
@@ -1,3 +1,5 @@
+# https://www.hackerrank.com/challenges/grading/problem
+
 # # HackerLand University has the following grading policy:
 
 # Every student receives a  in the inclusive range from  to .
