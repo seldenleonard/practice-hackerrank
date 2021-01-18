76b95aab6f18cb963a3a62764bca3a4d81a74abf
seldenleonard <selden222@gmail.com>
2021-01-17 21:17:07 -0500

    fixes typo in steps of pangram problem

--- ruby-problems/pangram-problem.rb
+++ ruby-problems/pangram-problem.rb
@@ -1,7 +1,7 @@
 # A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
 
 # STEPS
-# Create a methdo that takes in a string and searches for every letter of the alphabet it in it
+# Create a method that takes in a string and searches for every letter of the alphabet it in it
 
 def pangram?(str)
   
