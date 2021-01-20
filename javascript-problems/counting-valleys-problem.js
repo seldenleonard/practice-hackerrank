eea0605fd84e4310a6e48de02d9deba26546ca32
seldenleonard <selden222@gmail.com>
2021-01-20 06:17:09 -0500

    removes repition of defining function

--- javascript-problems/counting-valleys-problem.js
+++ javascript-problems/counting-valleys-problem.js
@@ -6,31 +6,29 @@
 // loop through and add or subract one based off of D or U
 
 function countingValleys(steps, path) {
-  function countingValleys(steps, path) {
-    var i = 0;
-    var seaLevel = 0;
-    var valleyCounter = 0;
-    if (path[i] === "D") {
-      console.log(`valley counter ${valleyCounter += 1}`);
-      console.log(`sea level counter ${seaLevel -= 1}`);
-      i += 1;
-    } else if (path[i] === "U") {
-      console.log(`sea level counter ${seaLevel += 1}`);
-      i += 1;
-    }
-    while (i < steps) {
-      if (path[i] === "D") {
-        console.log(`sea level counter in D ${seaLevel -= 1}`);
-      } else if (path[i] === "U") {
-        console.log(`sea level counter in U ${seaLevel += 1}`);
-      }
-      if (seaLevel === 0 && path[i + 1] === "D") {
-        console.log(`valley counter ${valleyCounter += 1}`);
-      }
-      i += 1;
-    }
-    return valleyCounter;
+  var i = 0;
+  var seaLevel = 0;
+  var valleyCounter = 0;
+  if (path[i] === "D") {
+    console.log(`valley counter ${valleyCounter += 1}`);
+    console.log(`sea level counter ${seaLevel -= 1}`);
+    i += 1;
+  } else if (path[i] === "U") {
+    console.log(`sea level counter ${seaLevel += 1}`);
+    i += 1;
   }
+  while (i < steps) {
+    if (path[i] === "D") {
+      console.log(`sea level counter in D ${seaLevel -= 1}`);
+    } else if (path[i] === "U") {
+      console.log(`sea level counter in U ${seaLevel += 1}`);
+    }
+    if (seaLevel === 0 && path[i + 1] === "D") {
+      console.log(`valley counter ${valleyCounter += 1}`);
+    }
+    i += 1;
+  }
+  return valleyCounter;
 }
 console.log(countingValleys(8, "UDDDUDUU"));
 
