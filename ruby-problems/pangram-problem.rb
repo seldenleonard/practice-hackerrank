6229c979f6d9f402e61ab583e1de58b2bf499df2
seldenleonard <selden222@gmail.com>
2021-01-26 12:51:25 -0500

    writes out steps for pangram-problem.rb

--- ruby-problems/pangram-problem.rb
+++ ruby-problems/pangram-problem.rb
@@ -1,7 +1,12 @@
 # A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
 
 # STEPS
-# Create a method that takes in a string and searches for every letter of the alphabet it in it
+# Create a method that takes in a string
+# Set alphabet = an array with all the letters of the alphabet
+# Create two indexes, one for alphabet array and one for input
+# Iterate once through alphabet for every time we iterate through all of input
+# Compare the current alphabet[index] to each value in the input
+# If all values in alphabet array match values in from the input, then return "pangram". If not, return "not pangram"
 
 def pangrams(s)
   
