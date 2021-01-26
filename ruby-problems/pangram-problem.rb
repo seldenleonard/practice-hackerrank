2da9393016c19863208986611ca7162bfe235d8a
seldenleonard <selden222@gmail.com>
2021-01-26 13:14:28 -0500

    turns input in pangrams-problem.rb into an array of individual characters

--- ruby-problems/pangram-problem.rb
+++ ruby-problems/pangram-problem.rb
@@ -9,12 +9,13 @@
 # If all values in alphabet array match values in from the input, then return "pangram". If not, return "not pangram"
 
 def pangrams(s)
-  s = s.gsub(/\s+/, "").downcase
+  s = s.gsub(/\s+/, "").downcase.split("")
   # Note: the forward slashes denotes a "regular expression", and the "\s+" refers to a space. Then the empty quotes tell us that we want to replace the space with nothing.
+  
   p s
 end
 
-p pangrams("Hi im Selden")
+pangrams("Hi im Selden")
 
 
 
