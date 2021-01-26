d3ac38620a38c1546f98e5e8da20f6dc8a770759
seldenleonard <selden222@gmail.com>
2021-01-26 13:47:09 -0500

    adds new step to steps

--- ruby-problems/pangram-problem.rb
+++ ruby-problems/pangram-problem.rb
@@ -2,7 +2,8 @@
 
 # STEPS
 # Create a method that takes in a string
-# Set alphabet = an array with all the letters of the alphabet
+# Set alphabet as an array with all the letters of the alphabet
+# Turn input string into an array, delete all spaces, and delete all non-unique letters
 # Create two indexes, one for alphabet array and one for input
 # Iterate once through alphabet for every time we iterate through all of input
 # Compare the current alphabet[index] to each value in the input
@@ -10,21 +11,22 @@
 
 def pangrams(s)
   alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  s = s.gsub(/\s+/, "").downcase.split("").uniq
+  s = s.gsub(/\s+/, "").downcase.split("").uniq.sort()
   # Note: the forward slashes denotes a "regular expression", and the "\s+" refers to a space. Then the empty quotes tell us that we want to replace the space with nothing.
   index1 = 0
   index2 = 0
   while index1 < alphabet.length
-      while index2 < s.length
-        if s[index2] = alphabet[index1]
-          p "match"
-        end
-        index2 += 1
-      end
+      # while index2 < s.length
+      #   if s[index2] == alphabet[index1]
+      #     p "match"
+      #   end
+      #   p s
+      #   index2 += 1
+      # end
     index1 += 1
   end
-  p alphabet
-  p s
+  # p alphabet
+  # p s
 end
 
 pangrams("Hi im Selden")
