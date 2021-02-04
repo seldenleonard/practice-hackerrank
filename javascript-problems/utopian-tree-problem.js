62f582cf8ddb6f55301ae8fec281c8a3376d6d00
seldenleonard <selden222@gmail.com>
2021-02-03 23:55:43 -0500

    fixes steps to solve utopian-tree-problem.js

--- javascript-problems/utopian-tree-problem.js
+++ javascript-problems/utopian-tree-problem.js
@@ -1,7 +1,7 @@
 // STEPS:
-// create a function that accepts the array "n"
-// set index to zero
-// set a variable for height, "h", equal to 1
-// create a while loop for when index is less than the length of array "n" 
+// Create a function that accepts the array "n"
+// Set index to zero
+// Set a variable for height, "h", equal to 1
+// Create a while loop for when index is less than the length of array "n" 
 //   Within the while loop, create an if/else statement where "h" gets doubled if the n[index] value is odd, or (else) h increases by 1 if n[index] is positive
-// return "h" for each value in array "n"
+// Return "h" for each value in array "n"
