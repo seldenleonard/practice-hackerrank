bffc6f0d719e5b3857312a269eba6524ca3ca577
seldenleonard <selden222@gmail.com>
2021-02-04 00:01:09 -0500

    adds link to hackerrank challenge

--- javascript-problems/utopian-tree-problem.js
+++ javascript-problems/utopian-tree-problem.js
@@ -1,3 +1,5 @@
+// https://www.hackerrank.com/challenges/utopian-tree/problem
+
 // STEPS:
 // Create a function that accepts the array "n"
 // Set index to zero
@@ -5,3 +7,4 @@
 // Create a while loop for when index is less than the length of array "n" 
 //   Within the while loop, create an if/else statement where "h" gets doubled if the n[index] value is odd, or (else) h increases by 1 if n[index] is positive
 // Return "h" for each value in array "n"
+
