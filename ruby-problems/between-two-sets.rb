af367fc7c6cbfec2ce6d505b4323827ed3272bb5
seldenleonard <selden222@gmail.com>
2021-02-11 03:33:48 -0500

    adds preliminary steps to solve a simplified version of between-two-sets.rb

--- ruby-problems/between-two-sets.rb
+++ ruby-problems/between-two-sets.rb
@@ -1,2 +1,6 @@
 # https://www.hackerrank.com/challenges/between-two-sets/problem
 
+# STEPS
+# Create a method that takes in two arrays
+# Create a loop that iterates through the second array
+#   Within this loop, create another loop that iterates through the first array and uses the modulo operator to determine if the integer being evaluated in the first array factors evenly into each value in the second array
