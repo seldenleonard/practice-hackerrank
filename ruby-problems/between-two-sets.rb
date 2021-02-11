6df1637452522a8b30ed396fefe43b5ca9be13b9
seldenleonard <selden222@gmail.com>
2021-02-11 03:42:16 -0500

    fixes steps and adds to them to solve a slightly more complex version of this problem

--- ruby-problems/between-two-sets.rb
+++ ruby-problems/between-two-sets.rb
@@ -2,5 +2,9 @@
 
 # STEPS
 # Create a method that takes in two arrays
+# Set an index equal to zero for the first array
+# Set an index equal to zero for the second array
 # Create a loop that iterates through the second array
 #   Within this loop, create another loop that iterates through the first array and uses the modulo operator to determine if the integer being evaluated in the first array factors evenly into each value in the second array
+# If the first array value does factor into each value in the second array, keep it, otherwise remove it from the first array -- so we are left with only the values in the first array that factor into all values of the secodn array 
+
