fa6463e917fbc3bbfcd0fe61bafa59e8adb0d67f
seldenleonard <selden222@gmail.com>
2021-02-25 02:22:03 -0800

    writes steps to solve cats and a mouse problem

--- ruby-problems/cats-and-a-mouse-problem.rb
+++ ruby-problems/cats-and-a-mouse-problem.rb
@@ -1,1 +1,8 @@
 # https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+
+# STEPS
+# Create a function that accepts an input of three integers, x (refers to Cat A position), y (refers to Cat B position), and z (refers to Mouse C position)
+# Compare the following to determine the output:
+# If the absolute value of the difference between x and z is smaller than the difference between y and z, Cat A catches the mouse first, so print Cat A.
+# If the absolute value of the difference between y and z is smaller than the difference between x and z, Cat A catches the mouse first, so print Cat A.
+# If the absolute value of the difference between y and z is equal to the difference between x and z, Mouse C escapes, so print Mouse C.
