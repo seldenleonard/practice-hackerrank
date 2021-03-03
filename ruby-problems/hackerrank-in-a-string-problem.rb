c4b3f6833ba2b5fda87606f0e86495fafad7cd63
seldenleonard <selden222@gmail.com>
2021-03-03 00:08:39 -0800

    adds preliminary steps to solve hackerrank in a string problem

--- ruby-problems/hackerrank-in-a-string-problem.rb
+++ ruby-problems/hackerrank-in-a-string-problem.rb
@@ -1,1 +1,8 @@
 # https://www.hackerrank.com/challenges/hackerrank-in-a-string/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+
+
+# STEPS
+# Create a function that accepts string 's'
+# Set a variable equal to an array containing each letter of the word 'hackerrank', in order
+# Set two indexes equal to zero (one for string 's', and one for the letters array)
+# Create two loops to iterate through string 's' and the letter array. Nest the loop using the index associated with string 's' inside of the loop using the index associated with the letter array.
