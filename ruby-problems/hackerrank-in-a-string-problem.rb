4c3fb3ce578c8003cbc0e3458e1ef145beff5761
seldenleonard <selden222@gmail.com>
2021-03-03 00:16:56 -0800

    completes writing steps to solve hackerrank in a string problem

--- ruby-problems/hackerrank-in-a-string-problem.rb
+++ ruby-problems/hackerrank-in-a-string-problem.rb
@@ -8,4 +8,4 @@
 # Create two loops to iterate through string 's' and the letter array. Nest the loop using the index associated with string 's' inside of the loop using the index associated with the letter array.
 # At each value in the letters array, iterate through string 's' until you find the matching letter.
 # Then iterate one value forward in the letters array.
-
+# Write an if statement, where if the string 's' index value reaches the length of string 's' before the letters array index reaches the length of the letters array, then return no. if vice versa, return yes.
