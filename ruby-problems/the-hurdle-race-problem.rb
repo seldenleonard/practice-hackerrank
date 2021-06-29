24cf2d8ff6d650b4cb42dc4714703f33423605d0
seldenleonard <selden222@gmail.com>
2021-06-29 00:02:34 -0700

    adds in integers in the-hurdle-race-problem.rb instructions, which were not transferred originally with copy and paste

--- ruby-problems/the-hurdle-race-problem.rb
+++ ruby-problems/the-hurdle-race-problem.rb
@@ -1,6 +1,6 @@
 # https://www.hackerrank.com/challenges/the-hurdle-race/problem
 
-# A video player plays a game in which the character competes in a hurdle race. Hurdles are of varying heights, and the characters have a maximum height they can jump. There is a magic potion they can take that will increase their maximum jump height by  unit for each dose. How many doses of the potion must the character take to be able to jump all of the hurdles. If the character can already clear all of the hurdles, return .
+# A video player plays a game in which the character competes in a hurdle race. Hurdles are of varying heights, and the characters have a maximum height they can jump. There is a magic potion they can take that will increase their maximum jump height by 1 unit for each dose. How many doses of the potion must the character take to be able to jump all of the hurdles. If the character can already clear all of the hurdles, return 0.
 
 # STEPS
 # Create a function that accepts an integer ("height") and an array ("k")
@@ -9,7 +9,3 @@
 # Iterate throught the array and if maxHeight is less than any value in the array, set maxHeight equal to that value
 # Define the variable "doses" as k - maxHeight
 # Create an if statement where if k is greater than or equal to maxHeight, return zero. If k is less than maxHeight, then return the value of "doses"
-
-def hurdleRace
-  
-end
