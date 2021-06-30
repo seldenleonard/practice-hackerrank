8c19e29ef37b6567183c03e4849a2b2c2a1bab68
seldenleonard <selden222@gmail.com>
2021-06-30 03:04:13 -0700

    cleans up and labels sections of designer-pdf-viewer-problem and adds link to problem on HackerRank website

--- ruby-problems/designer-pdf-viewer-problem.rb
+++ ruby-problems/designer-pdf-viewer-problem.rb
@@ -1,8 +1,11 @@
+# https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+
+# -- PROMPT --
 # When a contiguous block of text is selected in a PDF viewer, the selection is highlighted with a blue rectangle. In this PDF viewer, each word is highlighted independently.
 
 # There is a list of  character heights aligned by index to their letters. For example, 'a' is at index  and 'z' is at index . There will also be a string. Using the letter heights given, determine the area of the rectangle highlight in  assuming all letters are  wide.
 
-# STEPS
+# -- STEPS --
 # Create a method that takes in an array of 26 integers and a string
 # Create a variable called letter_location and set it equal to a hash containing key-value pairings with each letter of the alphabet corresponding to a value (which will then correspond to the index location of values in the integer input array). Alphabetically, "a" will equal "0", "b" will equal "1", etc.
 # Evaluate each letter in the input string, use letter_location to find the index value, then take that index value and retrieve the corresponding integer within the input array
@@ -22,6 +25,8 @@ def designerPdfViewer(h, word)
   p max_letter_height * word.length
 end
 
+# -- TEST CASE 1 --
 # designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7], "zaba")
 
+# -- TEST CASE 2 --
 designerPdfViewer([6, 3, 6, 3, 1, 7, 3, 7, 1, 6, 7, 4, 7, 4, 3, 2, 4, 2, 5, 4, 3, 6, 7, 6, 2, 5], "tehwrcxhnq")
