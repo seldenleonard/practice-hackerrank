13a4963d6bcbafcdd15ee0e5ec23e96ac6b29520
seldenleonard <selden222@gmail.com>
2021-06-30 01:25:56 -0700

    completes first attempt at designer-pdf-viewer-problem.rb

--- ruby-problems/designer-pdf-viewer-problem.rb
+++ ruby-problems/designer-pdf-viewer-problem.rb
@@ -9,5 +9,12 @@
 # Multiply this integer by the length of the string, and that is the output
 
 def designerPdfViewer(h, word)
-  
+  letter_location = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8, j: 9, k: 10, l: 11, m: 12, n: 13, o: 14, p: 15, q: 16, r: 17, s: 18, t: 19, u: 20, v: 21, w: 22, x: 23, y: 24, z: 25 }
+  i = 0
+  max_letter_height = 0
+  while i < word.length
+    if letter_location[word[i]] > max_letter_height
+      max_letter_height = letter_location[word[i]]   
+    end
+  end
 end
