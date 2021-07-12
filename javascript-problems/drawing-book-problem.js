0e9698b83445ee08089f21a9a5d1e80ca085478a
seldenleonard <selden222@gmail.com>
2021-07-11 23:45:33 -0400

    cleans up bill-division-problem.js steps section

--- javascript-problems/drawing-book-problem.js
+++ javascript-problems/drawing-book-problem.js
@@ -2,11 +2,11 @@
 
 // STEPS
 
-// write conditional that says if n divided by 2 is less than p start from the back of the book
-//   if n divided by 2 is more than p, start from the front of the book
-// when starting at back of book, if total number of pages is even, first subract 1 and add 1 to a page counter, then subtract 2 and add 1 to page counter until you get to the page number
-// when starting at the front of the book, add 2 to the index and add 1 to the counter until you get to the page number
-// print counter
+// Write conditional that says if n divided by 2 is less than p start from the back of the book
+//  --- If n divided by 2 is more than p, start from the front of the book
+// When starting at back of book, if total number of pages is even, first subract 1 and add 1 to a page counter, then subtract 2 and add 1 to page counter until you get to the page number
+// When starting at the front of the book, add 2 to the index and add 1 to the counter until you get to the page number
+// Print counter
 
 function pageCount(n, p) {
   let counter = 0;
