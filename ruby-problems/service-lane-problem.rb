9e12ce725d4043426748cc90151b04971adf21a4
seldenleonard <selden222@gmail.com>
2021-07-16 02:00:37 -0400

    adds link to service-lane-problem on hackerrank

--- ruby-problems/service-lane-problem.rb
+++ ruby-problems/service-lane-problem.rb
@@ -1,3 +1,5 @@
+# https://www.hackerrank.com/challenges/service-lane/problem
+
 # A driver is driving on the freeway. The check engine light of his vehicle is on, and the driver wants to get service immediately. Luckily, a service lane runs parallel to the highway. It varies in width along its length.
 # You will be given an array of widths at points along the road (indices), then a list of the indices of entry and exit points. Considering each entry and exit point pair, calculate the maximum size vehicle that can travel that segment of the service lane safely.
 
@@ -42,19 +44,19 @@
 # end
 
 # ATTEMPT 3 - Correct -- BUT HackerRank Doesnt Like It
-  index1 = 0
-  while index1 < cases.length
-    index2 = cases[index1][0]
-    smallest_width = width[index2]
-    while index2 <= cases[index1][1]
-      if smallest_width > width[index2]
-        smallest_width = width[index2]
-      end
-      index2 += 1
-    end
-    p smallest_width
-    index1 += 1
-  end
-end
+#   index1 = 0
+#   while index1 < cases.length
+#     index2 = cases[index1][0]
+#     smallest_width = width[index2]
+#     while index2 <= cases[index1][1]
+#       if smallest_width > width[index2]
+#         smallest_width = width[index2]
+#       end
+#       index2 += 1
+#     end
+#     p smallest_width
+#     index1 += 1
+#   end
+# end
 
 serviceLane([2, 3, 1, 2, 3, 2, 3, 3], [[0, 3], [4, 6], [6, 7], [3, 5], [0, 7]])
