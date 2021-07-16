6465a514cc9da0bf1a6ca1c1857862bc3747ee7e
seldenleonard <selden222@gmail.com>
2021-07-16 01:57:54 -0400

    creates second attempt at service-lane-problem.rb

--- ruby-problems/service-lane-problem.rb
+++ ruby-problems/service-lane-problem.rb
@@ -8,18 +8,37 @@
 # Using each pair of integers in each subarray as index values for the width array, return the smallest value in the width array, inside of that index range
 
 # ATTEMPT 1 - Forgot to Utilize 'width' Input
+# def serviceLane(width, cases)
+#   index1 = 0
+#   while index1 < cases.length
+#     index2 = cases[index1][0]
+#     smallest_width = index2
+#     while index2 <= cases[index1][1]
+#       if smallest_width > cases[index1][index2]
+#         smallest_width = cases[index1][index2]
+#       end
+#       p smallest_width
+#       index2 += 1
+#     end
+#     index1 += 1
+#   end
+# end
+
+# ATTEMPT 2 - Outputs Correctly 3/5 Integers Expected
 def serviceLane(width, cases)
   index1 = 0
   while index1 < cases.length
     index2 = cases[index1][0]
     smallest_width = index2
     while index2 <= cases[index1][1]
-      if smallest_width > cases[index1][index2]
-        smallest_width = cases[index1][index2]
+      if smallest_width > width[index2]
+        smallest_width = width[index2]
       end
-      p smallest_width
       index2 += 1
     end
+    p smallest_width
     index1 += 1
   end
 end
+
+serviceLane([2, 3, 1, 2, 3, 2, 3, 3], [[0, 3], [4, 6], [6, 7], [3, 5], [0, 7]])
