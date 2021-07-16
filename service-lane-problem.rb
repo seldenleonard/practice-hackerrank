fa2355ea6550d46f15c50674dd3ee7186f2ad941
seldenleonard <selden222@gmail.com>
2021-07-15 22:51:51 -0400

    adds prompt to service-lane-problem.rb

---
+++ service-lane-problem.rb
@@ -0,0 +1,2 @@
+# A driver is driving on the freeway. The check engine light of his vehicle is on, and the driver wants to get service immediately. Luckily, a service lane runs parallel to the highway. It varies in width along its length.
+# You will be given an array of widths at points along the road (indices), then a list of the indices of entry and exit points. Considering each entry and exit point pair, calculate the maximum size vehicle that can travel that segment of the service lane safely.
