2c44471c15d9a13bef5a73623ed4eb10a0718b7c
seldenleonard <selden222@gmail.com>
2021-07-15 23:02:27 -0400

    writes out steps for service-lane-problem.rb

--- ruby-problems/service-lane-problem.rb
+++ ruby-problems/service-lane-problem.rb
@@ -2,3 +2,7 @@
 # You will be given an array of widths at points along the road (indices), then a list of the indices of entry and exit points. Considering each entry and exit point pair, calculate the maximum size vehicle that can travel that segment of the service lane safely.
 
 # STEPS
+# Create a method that accepts:
+# --- Width: An array of integers
+# --- Cases: An array of arrays, each sub-array containing 2 integer values
+# Using each pair of integers in each subarray as index values for the width array, return the smallest value in the width array, inside of that index range
