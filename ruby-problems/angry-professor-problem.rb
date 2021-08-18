1cf56f529db1432bbca3e685ea9fa2f9347e08fa
seldenleonard <selden222@gmail.com>
2021-08-18 19:36:49 -0400

    adds link to hackerrank challenge in angry-professor-problem.rb

--- ruby-problems/angry-professor-problem.rb
+++ ruby-problems/angry-professor-problem.rb
@@ -1,2 +1,4 @@
+# https://www.hackerrank.com/challenges/angry-professor/problem
+
 # A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, the professor decides to cancel class if fewer than some number of students are present when class starts. Arrival times go from on time (arrivalTime >= 0) to arrived late (arrivalTime > 0).
 # Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled.
