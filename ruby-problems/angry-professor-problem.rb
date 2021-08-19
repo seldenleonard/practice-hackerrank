4f88882fb5d74fa660553f780eb50d81b7f05933
seldenleonard <selden222@gmail.com>
2021-08-19 02:20:39 -0400

    creates first attempt at solving angry-profesor-problem.rb and edits steps

--- ruby-problems/angry-professor-problem.rb
+++ ruby-problems/angry-professor-problem.rb
@@ -7,9 +7,20 @@
 # Create a method that accepts 2 values: integer k and array a
   # integer k = the threshold number of students needed on time for class to not be cancelled
   # array a = the arrival times of each student
-# Create an each or while loop that iterates through array a, and counts each student that is late
+# Create an each or while loop that iterates through array a, and counts each student that is late, using an if statement
 # Write an if-statement, saying that if the number of students who are late exceeds the threshold, return "yes". Otherwise return "no".
 
 def angryProfessor(k, a)
-  
+  i = 0
+  late = 0
+  a.each |arrival| do
+    if arrival > 0
+      late += 1
+    end
+  end
+  if a.length - late >= k
+    p "YES"
+  else
+    p "NO"
+  end
 end
