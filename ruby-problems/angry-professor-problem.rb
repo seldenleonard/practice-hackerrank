11156a5861c270944e881ee5f371e3cf9427ae04
seldenleonard <selden222@gmail.com>
2021-08-19 02:13:46 -0400

    corrects steps to reflect desired method inputs

--- ruby-problems/angry-professor-problem.rb
+++ ruby-problems/angry-professor-problem.rb
@@ -4,9 +4,13 @@
 # Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled.
 
 # STEPS
-# Create a method that accepts any number of test cases (t), and within each test case, there are 3 values: 2 integers and an array
+# Create a method that accepts 3 values: 2 integers and an array
   # integer n = number of students in the class
   # integer k = the threshold number of students needed on time for class to not be cancelled
   # array a = the arrival times of each student
 # Create an each or while loop that iterates through array a, and counts each student that is late
-# Write an if-statement, saying that if the number of students who are late exceeds the threshold, return "false". Otherwise return "true".
+# Write an if-statement, saying that if the number of students who are late exceeds the threshold, return "yes". Otherwise return "no".
+
+def angryProfessor(k, a)
+  
+end
