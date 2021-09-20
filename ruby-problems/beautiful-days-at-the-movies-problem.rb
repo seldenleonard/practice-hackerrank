e1eecc893b28429e118dbf5d26fe39cf4158e04a
seldenleonard <selden222@gmail.com>
2021-09-20 04:06:51 -0700

    adds steps to solve beautiful-days-at-the-movies-problem.rb

--- ruby-problems/beautiful-days-at-the-movies-problem.rb
+++ ruby-problems/beautiful-days-at-the-movies-problem.rb
@@ -6,3 +6,14 @@
 # She decides to apply her game to decision making. She will look at a numbered range of days and will only go to a movie on a beautiful day.
 
 # Given a range of numbered days, [i...j] and a number k, determine the number of days in the range that are beautiful. Beautiful numbers are defined as numbers where |i-reverse(i)| is evenly divisible by k. If a day's value is a beautiful number, it is a beautiful day. Return the number of beautiful days in the range.
+
+# STEPS
+# Create a method that accepts three integers (i: the starting day number; j: the ending day number; & k: the divisor)
+# Subtract i from d to find the total number of days in the range
+# Write a loop that goes the length of the total number of days in the range
+  # Within this loop, do the following actions/calculations
+  # Reverse the individual values for the integer within the range that is being evaluated, and define this new integer as 'reverse'
+  # Then take the original integer and subtract its reverse integer
+  # Then divide the result by k
+  # If the resulting value is a whole number integer, then add one to a counter of beautiful days, otherwise, do not add one to this counter and simply let the loop continue
+# Once the loop has ran its course, output the number of beautiful days
