1c655a6e9c59953458375f12e95896de765e3f66
seldenleonard <selden222@gmail.com>
2021-10-12 03:07:47 -0700

    adds link to viral advertising challenge on hackerrank.com

--- ruby-problems/viral-advertising-problem.rb
+++ ruby-problems/viral-advertising-problem.rb
@@ -1,3 +1,5 @@
+# https://www.hackerrank.com/challenges/strange-advertising/problem
+
 # HackerLand Enterprise is adopting a new viral advertising strategy. When they launch a new product, they advertise it to exactly 5 people on social media.
 
 # On the first day, half of those 5 people (i.e., floor(5/2) = 2) like the advertisement and each shares it with 3 of their friends. At the beginning of the second day, floor(5/2) * 3 = 2 * 3 = 6 people receive the advertisement.
