a9565a4fe778d2c8fcfb319f98970e7827042615
seldenleonard <selden222@gmail.com>
2021-10-12 03:09:12 -0700

    cleans up formatting and adds label to ech section in viral-advertising-problem.rb

--- ruby-problems/viral-advertising-problem.rb
+++ ruby-problems/viral-advertising-problem.rb
@@ -1,10 +1,13 @@
 # https://www.hackerrank.com/challenges/strange-advertising/problem
 
+
+# CHALLENGE INSTRUCTIONS
 # HackerLand Enterprise is adopting a new viral advertising strategy. When they launch a new product, they advertise it to exactly 5 people on social media.
 
 # On the first day, half of those 5 people (i.e., floor(5/2) = 2) like the advertisement and each shares it with 3 of their friends. At the beginning of the second day, floor(5/2) * 3 = 2 * 3 = 6 people receive the advertisement.
 
 # Each day, floor(recipients/2) of the recipients like the advertisement and will share it with 3 friends on the following day. Assuming nobody receives the advertisement twice, determine how many people have liked the ad by the end of a given day, beginning with launch day as day 1.
+
 
 # STEPS
 # Create a method that accepts a single integer "n", which is the number of days
@@ -13,6 +16,7 @@
   # Within this loop, redefine the variables as follows:
     # day += 1, shared = liked * 3, liked = shared / 2, & cumulative += liked
 # Return cumulative
+
 
 def viralAdvertising(n)
   day = 1
