fd2be72558f0a5a65cb01ac7ef74ec90911d1f92
seldenleonard <selden222@gmail.com>
2021-10-27 04:55:03 -0700

    creates second attempt to solve sales-by-match-problem.rb

--- ruby-problems/sales-by-match-problem.rb
+++ ruby-problems/sales-by-match-problem.rb
@@ -21,25 +21,51 @@
     # Inside this loop, have "index2" iterate through "ar" to find a number equal to ar[index]. If there is a number, remove both ar[index] and ar[index2] from ar, then add 1 to "pairs". If not, then just move on
   # Return "pairs"
 
+# def sockMerchant(ar)
+#   pairs = 0
+#   index = 0
+#   index2 = 0
+#   # ar.sort
+#   while index < ar.length
+#     while index2 < ar.length
+#       if index2 == ar[index]
+#         pairs += 1
+#         ar.delete_at(index)
+#         ar.delete_at(index2)
+#         index2 == ar.length
+#       end
+#       index2 += 1
+#     end
+#     index += 1
+#     index2 = 0
+#   end
+#   pairs
+# end
+
 def sockMerchant(ar)
   pairs = 0
   index = 0
   index2 = 0
-  ar.sort
   while index < ar.length
     while index2 < ar.length
-      if index2 == ar[index]
+      index2 += 1
+        # p "index2"
+        # p ar[index2]
+        # p "index"
+        # p ar[index]
+      if ar[index2] == ar[index]
         pairs += 1
-        ar.delete_at(index)
+        # ar.delete_at(index)
         ar.delete_at(index2)
         index2 == ar.length
       end
-      index2 += 1
     end
-    index += 1
-    index2 = 0
+    # index += 1
+    ar.delete_at(index)
+    index2 = index + 1
   end
   pairs
 end
 
 p sockMerchant([1, 2, 1, 2, 1, 3, 2])
+p sockMerchant([10, 20, 20, 10, 10, 30, 50, 10, 20])
