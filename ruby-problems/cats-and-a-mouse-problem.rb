a1b830773644edd9233d51890a06b4637a2ee3d1
seldenleonard <selden222@gmail.com>
2021-11-20 00:47:15 -0800

    Adds instructions to cats-and-a-mouse.rb file

--- ruby-problems/cats-and-a-mouse-problem.rb
+++ ruby-problems/cats-and-a-mouse-problem.rb
@@ -1,5 +1,14 @@
 # https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
 
+# INSTRUCTIONS
+# Two cats and a mouse are at various positions on a line. You will be given their starting positions. Your task is to determine which cat will reach the mouse first, assuming the mouse does not move and the cats travel at equal speed. If the cats arrive at the same time, the mouse will be allowed to move and it will escape while they fight.
+
+# You are given q queries in the form of x, y, and z representing the respective positions for cats A and B, and for mouse C. Complete the function catAndMouse to return the appropriate answer to each query, which will be printed on a new line.
+
+# If cat A catches the mouse first, print Cat A.
+# If cat B catches the mouse first, print Cat B.
+# If both cats reach the mouse at the same time, print Mouse C as the two cats fight and mouse escapes.
+
 # STEPS
 # Create a function that accepts an input of three integers, x (refers to Cat A position), y (refers to Cat B position), and z (refers to Mouse C position)
 # Compare the following to determine the output:
