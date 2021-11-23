5f8e8f40c1cc4d8e819b318e4c0838c585361377
seldenleonard <selden222@gmail.com>
2021-11-22 22:26:08 -0800

    Adds whiteboarding steps to solve find-digits-problem.rb

--- ruby-problems/find-digits-problem.rb
+++ ruby-problems/find-digits-problem.rb
@@ -3,3 +3,8 @@
 # Given an integer, for each digit that makes up the integer determine whether it is a divisor. Count the number of divisors occurring within the integer.
 
 # STEPS
+# Create a function that accepts a single integer
+# Set a counter equal to zero
+# Loop through each digit of the integer and if the digit in question divides evenly (use modulo operator) into the input integer, then add 1 to the counter
+# Return the counter
+
