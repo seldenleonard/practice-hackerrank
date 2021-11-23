e0fe109e8883cbd7ec36ae1ae29812cd0909917c
seldenleonard <selden222@gmail.com>
2021-11-22 23:31:43 -0800

    writes out steps for prisoners-candy-problem.rb

---
+++ prisoners-candy.rb
@@ -0,0 +1,13 @@
+# STEPS
+# Create a method that accepts 3 integers
+  # n = number of prisoners
+  # m = number of candies
+  # s = seat number that is first to recieve candy 
+# Write a loop that ends when candies are all handed out
+  # Inside this loop, write an if statement that resets to seat number 1 after the last seat number has recieved candy
+  # Return the seat number of who recieved the last candy
+
+def prisonerCandy(n, m, s)
+  
+  
+end
