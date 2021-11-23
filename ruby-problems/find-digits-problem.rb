b29af093d1594a4c534e45adf3d1a3fad97604d2
seldenleonard <selden222@gmail.com>
2021-11-22 23:15:02 -0800

    adds link to find digits problem on HackerRank site

--- ruby-problems/find-digits-problem.rb
+++ ruby-problems/find-digits-problem.rb
@@ -1,3 +1,5 @@
+# https://www.hackerrank.com/challenges/find-digits/problem?isFullScreen=true
+
 # INSTRUCTIONS
 # An integer d is a divisor of an integer n if the remainder of n/d = 0.
 # Given an integer, for each digit that makes up the integer determine whether it is a divisor. Count the number of divisors occurring within the integer.
