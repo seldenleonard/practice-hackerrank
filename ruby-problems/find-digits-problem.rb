55c24327bea542fce49f9b0ace6ae08588b6de3c
seldenleonard <selden222@gmail.com>
2021-11-22 22:41:36 -0800

    writes first iteration of find digits problem method according to whiteboarding steps

--- ruby-problems/find-digits-problem.rb
+++ ruby-problems/find-digits-problem.rb
@@ -4,7 +4,18 @@
 
 # STEPS
 # Create a function that accepts a single integer
-# Set a counter equal to zero
+# Set a variable called 'counter' equal to zero
 # Loop through each digit of the integer and if the digit in question divides evenly (use modulo operator) into the input integer, then add 1 to the counter
 # Return the counter
 
+def findDigits(n)
+  counter = 0
+  i = 0
+  while i < n.length
+    if n / n[i] == 0 # Actually I dont think I do need to use modulo operator
+      counter += 1
+    end
+    i += 1
+  end
+  counter
+end
