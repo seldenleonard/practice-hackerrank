69acccb0d62b04a599e7e8a2019990afa10294e5
seldenleonard <selden222@gmail.com>
2021-12-07 01:30:20 -0800

    adds instructions title to service-lane-problem.rb

--- ruby-problems/service-lane-problem.rb
+++ ruby-problems/service-lane-problem.rb
@@ -1,5 +1,6 @@
 # https://www.hackerrank.com/challenges/service-lane/problem
 
+# INSTRUCTIONS
 # A driver is driving on the freeway. The check engine light of his vehicle is on, and the driver wants to get service immediately. Luckily, a service lane runs parallel to the highway. It varies in width along its length.
 # You will be given an array of widths at points along the road (indices), then a list of the indices of entry and exit points. Considering each entry and exit point pair, calculate the maximum size vehicle that can travel that segment of the service lane safely.
 
