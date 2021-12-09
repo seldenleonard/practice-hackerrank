a2cb017ebe9555ce833153ae755aed272d5f2b48
seldenleonard <selden222@gmail.com>
2021-12-09 02:03:06 -0800

    adds instructions to apples-and-oranges-problem.rb file and fills in variables where they would not paste over from HackerRank

--- ruby-problems/apples-and-oranges-problem.rb
+++ ruby-problems/apples-and-oranges-problem.rb
@@ -1,5 +1,16 @@
 # https://www.hackerrank.com/challenges/apple-and-orange/problem
 
+# INSTRUCTIONS
+# Sam's house has an apple tree and an orange tree that yield an abundance of fruit. Using the information given below, determine the number of apples and oranges that land on Sam's house.
+
+# In the diagram below:
+  # The red region denotes the house, where s is the start point, and t is the endpoint. The apple tree is to the left of the house, and the orange tree is to its right.
+  # Assume the trees are located on a single point, where the apple tree is at point a, and the orange tree is at point b.
+  # When a fruit falls from its tree, it lands d units of distance from its tree of origin along the x-axis.
+  # **A negative value of d means the fruit fell d units to the tree's left, and a positive value of d means it falls d units to the tree's right.**
+
+  # Given the value of d for m apples and n oranges, determine how many apples and oranges will fall on Sam's house (i.e., in the inclusive range [s,t])?
+
 # STEPS
 # map through apples and add position of tree to apple distance
 # map through oranges and add position of tree to orange distance
