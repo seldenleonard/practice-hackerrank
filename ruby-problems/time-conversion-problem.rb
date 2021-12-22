7a2633e01562de6b3fe92a7f954d57a4357b6eaa
seldenleonard <selden222@gmail.com>
2021-12-22 04:17:36 -0500

    Adds instructions to time conversion problem

--- ruby-problems/time-conversion-problem.rb
+++ ruby-problems/time-conversion-problem.rb
@@ -1,9 +1,11 @@
 # https://www.hackerrank.com/challenges/time-conversion/problem
 
+# INSTRUCTIONS
 # Given a time in -hour AM/PM format, convert it to military (24-hour) time.
 
 # Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
 # - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
 
 # STEPS
 # AM
