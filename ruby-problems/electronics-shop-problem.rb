02d303542474fbb18e74b37b9c854a301c8e674a
seldenleonard <selden222@gmail.com>
2022-01-12 04:37:35 -0500

    Adds label in electronics shop problem

--- ruby-problems/electronics-shop-problem.rb
+++ ruby-problems/electronics-shop-problem.rb
@@ -1,5 +1,6 @@
 # https://www.hackerrank.com/challenges/electronics-shop/problem?isFullScreen=true
 
+# INSTRUCTIONS
 # A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a give budget. Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is not possible to buy both items, return -1.
 
 # Function Description
