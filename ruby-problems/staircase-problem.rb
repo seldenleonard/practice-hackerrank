0958ce8bd3de3ef6f38110ac4032250b3db60a2e
seldenleonard <selden222@gmail.com>
2022-01-16 23:50:24 -0800

    fixes up formatting

--- ruby-problems/staircase-problem.rb
+++ ruby-problems/staircase-problem.rb
@@ -8,7 +8,6 @@
 # # # #
 
 # Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
-
 # Write a program that prints a staircase of size n.
 
 # STEPS
