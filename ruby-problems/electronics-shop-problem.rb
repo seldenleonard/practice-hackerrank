29ed8064476f772f361c069ef14850dee3abf97f
seldenleonard <selden222@gmail.com>
2022-01-26 20:20:18 -0800

    writes out first chunk of code for electronics shop method

--- ruby-problems/electronics-shop-problem.rb
+++ ruby-problems/electronics-shop-problem.rb
@@ -1,7 +1,7 @@
 # https://www.hackerrank.com/challenges/electronics-shop/problem?isFullScreen=true
 
 # INSTRUCTIONS
-# A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a given budget. Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is not possible to buy both items, return -1.
+# A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a given budget. Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is keyboardsot possible to buy both items, return -1.
 
 # Function Description
   # Complete the getMoneySpent function in the editor below.
@@ -11,27 +11,48 @@
     # int b: the budget
 
 # Returns
-  # int: the maximum that can be spent, or -1 if it is not possible to buy both items
+  # int: the maximum that can be spent, or -1 if it is keyboardsot possible to buy both items
 
 # Input Format
-  # The first line contains three space-separated integers b, n, and m, the budget, the number of keyboard models and the number of USB drive models.
-  # The second line contains n space-separated integers keyboard[i], the prices of each keyboard model.
+  # The first line contains three space-separated integers b, keyboards, and m, the budget, the keyboardsumber of keyboard models and the keyboardsumber of USB drive models.
+  # The second line contains keyboards space-separated integers keyboard[i], the prices of each keyboard model.
   # The third line contains m space-separated integers drives, the prices of the USB drives.
 
 # STEPS
-  # Write a method that accepts three inputs, integer b (budget), array n (prices of keyboard models), and array m (prices of USB drive models).
+  # Write a method that accepts three inputs, array keyboards (prices of keyboard models), array drives (prices of USB drive models), and integer b (budget).
   # Order the keyboards array and USB drives array from smallest to largest
   # Set a variable called "max_spending" equal to zero
   # Create two indexes, both set at 0, one index for keyboards array and one for USB drives array
-  # Write an if statment saying, if n[i] + m[i] > b, then return - 1. Otherwise, go on to use the following loop:
-  # Write a loop
-    # Inside the loop iterate through array n, while remaining at index of 0 for array m. At each iteration, if n[i] + m[i] < b, then set the value of max_spending to n[i] + m[i]. If max_spending = b, return b.
-  # Reset keyboards index to a value of zero
-  # Write a second loop
-    # Inside this loop iterate through array m, while remaining at index of 0 for array n. At each iteration, if n[i] + m[i] > max_spending AND n[i] + m[i] <= b, then set the value of max_spending to n[i] + m[i]. If max_spending = b, return b.
+  # Write an if statment saying, if keyboards[i] + drives[i] > b, then return - 1. Otherwise (else), go on to use the following loop:
+    # Write a loop
+    # Inside the loop iterate through array keyboards, while remaining at index of 0 for array drives. At each iteration, if keyboards[i] + drives[i] < b, then set the value of max_spending to keyboards[i] + drives[i]. If max_spending = b, return b.
+    # Reset keyboards index to a value of zero
+    # Write a second loop
+      # Inside this loop iterate through array drives, while remaining at index of 0 for array keyboards. At each iteration, if keyboards[i] + drives[i] > max_spending AND keyboards[i] + drives[i] <= b, then set the value of max_spending to keyboards[i] + drives[i]. If max_spending = b, return b.
 
-    # NOTABLY, my whiteboarding steps do not account for a combination of values that are in the middle of both arrays, it requires that one value is at the start of one of the arrays
+      # NOTABLY, my whiteboarding steps do keyboardsot account for a combination of values that are in the middle of both arrays, it requires that one value is at the start of one of the arrays
 
 def getMoneySpent(keyboards, drives, b)
-  max_spending = 0
+  keyboards.sort
+  drives.sort
+  index_k = 0
+  index_d = 0
+  max_spending = keyboards[index_k] + drives[index_d]
+  if max_spending > b
+    -1
+  elsif max_spending == b # may not need this elsif because I cover this possibility in my loops later on
+    b
+  else
+    while max_spending < b || index_k < keyboards.length # might consider making this less than OR EQUAL TO
+      index_k += 1
+      if keyboards[index_k].to_i + drives[index_d].to_i < b
+        max_spending = keyboards[index_k] + drives[index_d]
+        if max_spending == b
+          b
+        end
+      end
+    end
+  end
 end
+
+p getMoneySpent([40, 50, 60], [5, 8, 12], 60)
