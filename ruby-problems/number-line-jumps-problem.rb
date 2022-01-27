32a97a6259de8fb662a94ba0a03bd7c63114a98b
seldenleonard <selden222@gmail.com>
2022-01-26 20:58:43 -0800

    adds instructions label

--- ruby-problems/number-line-jumps-problem.rb
+++ ruby-problems/number-line-jumps-problem.rb
@@ -1,5 +1,6 @@
 # https://www.hackerrank.com/challenges/kangaroo/problem
 
+# INSTRUCTIONS
 # You are choreographing a circus show with various animals. For one act, you are given two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity).
 # The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump.
 # The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
