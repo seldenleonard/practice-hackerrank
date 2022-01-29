fce6d81b619a4f1598f4d8cc88335c83917e21bf
seldenleonard <selden222@gmail.com>
2022-01-29 00:32:30 -0800

    fixes more typos

--- ruby-problems/electronics-shop-problem.rb
+++ ruby-problems/electronics-shop-problem.rb
@@ -1,7 +1,7 @@
 # https://www.hackerrank.com/challenges/electronics-shop/problem?isFullScreen=true
 
 # INSTRUCTIONS
-# A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a given budget. Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is keyboardsot possible to buy both items, return -1.
+# A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a given budget. Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is not possible to buy both items, return -1.
 
 # Function Description
   # Complete the getMoneySpent function in the editor below.
@@ -14,12 +14,12 @@
   # int: the maximum that can be spent, or -1 if it is keyboardsot possible to buy both items
 
 # Input Format
-  # The first line contains three space-separated integers b, keyboards, and m, the budget, the keyboardsumber of keyboard models and the keyboardsumber of USB drive models.
-  # The second line contains keyboards space-separated integers keyboard[i], the prices of each keyboard model.
+  # The first line contains three space-separated integers b, n, and m, the budget, the number of keyboard models and the number of USB drive models.
+  # The second line contains n space-separated integers keyboard[i], the prices of each keyboard model.
   # The third line contains m space-separated integers drives, the prices of the USB drives.
 
 # STEPS
-  # Write a method that accepts three inputs, array keyboards (prices of keyboard models), array drives (prices of USB drive models), and integer b (budget).
+  # Write a method that accepts three inputs, array keyboard (prices of keyboard models), array drives (prices of USB drive models), and integer b (budget).
   # Order the keyboards array and USB drives array from smallest to largest
   # Set a variable called "max_spending" equal to zero
   # Create two indexes, both set at 0, one index for keyboards array and one for USB drives array
