0a91fa22d457c7a61696c3ca17d2e0157fdd8764
seldenleonard <selden222@gmail.com>
2022-06-28 23:29:46 -0700

    write out whiteboarding steps

--- ruby-problems/cut-the-sticks.rb
+++ ruby-problems/cut-the-sticks.rb
@@ -1,3 +1,12 @@
 # INSTRUCTIONS
 # You are given a number of sticks of varying lengths. You will iteratively cut the sticks into smaller sticks, discarding the shortest pieces until there are none left. At each iteration you will determine the length of the shortest stick remaining, cut that length from each of the longer sticks and then discard all the pieces of that shortest length. When all the remaining sticks are the same length, they cannot be shortened so discard them.
 # Given the lengths of n sticks, print the number of sticks that are left before each iteration until there are none left.
+
+# STEPS
+# Define a method that accepts an array
+# Order array from smallest to largest
+# Write a loop that iterates through the array
+  # Loop through array, print array.length, subtract array[0] from each value in the array, then remove any zero values
+  # When array.length = 0, break the loop
+
+# TRY WITH UNTIL LOOP AFTER
