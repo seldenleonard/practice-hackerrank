09056bca614a72879a7025e263696185a0952da6
seldenleonard <selden222@gmail.com>
2022-06-28 23:40:33 -0700

    fixes method and index variables to reflect proper ruby naming conventions

--- ruby-problems/sales-by-match-problem.rb
+++ ruby-problems/sales-by-match-problem.rb
@@ -13,59 +13,59 @@
 # Create a method that accepts array "ar"
 # Define variable "pairs" as equal to zero
 # Define variable 'index" as equal to zero
-# Define variable 'index2" as equal to zero
+# Define variable 'index_2" as equal to zero
 # Create a loop ends that ends when "index" = "ar" - 1
   # Iterate through the loop using "index"
-  # Create another loop that ends when "index2" = "ar" - 1
-    # Inside this loop, have "index2" iterate through "ar" to find a number equal to ar[index]. If there is a number, remove both ar[index] and ar[index2] from ar, then add 1 to "pairs". If not, then just move on
+  # Create another loop that ends when "index_2" = "ar" - 1
+    # Inside this loop, have "index_2" iterate through "ar" to find a number equal to ar[index]. If there is a number, remove both ar[index] and ar[index_2] from ar, then add 1 to "pairs". If not, then just move on
   # Return "pairs"
 
-# def sockMerchant(ar)
+# def sock_merchant(ar)
 #   pairs = 0
 #   index = 0
-#   index2 = 0
+#   index_2 = 0
 #   # ar.sort
 #   while index < ar.length
-#     while index2 < ar.length
-#       if index2 == ar[index]
+#     while index_2 < ar.length
+#       if index_2 == ar[index]
 #         pairs += 1
 #         ar.delete_at(index)
-#         ar.delete_at(index2)
-#         index2 == ar.length
+#         ar.delete_at(index_2)
+#         index_2 == ar.length
 #       end
-#       index2 += 1
+#       index_2 += 1
 #     end
 #     index += 1
-#     index2 = 0
+#     index_2 = 0
 #   end
 #   pairs
 # end
 
 # THIS WAS MY ATTEMPT TO SOLVE IT ON THE PLANE WITH NO INTERNET AND IT WORKED FOR MY TEST CASES BUT STILL FAILED SOME HACKERRANK TEST CASES
-def sockMerchant(ar)
+def sock_merchant(ar)
   pairs = 0
   index = 0
-  index2 = 0
+  index_2 = 0
   while index < ar.length
-    while index2 < ar.length
-      index2 += 1
-        # p "index2"
-        # p ar[index2]
+    while index_2 < ar.length
+      index_2 += 1
+        # p "index_2"
+        # p ar[index_2]
         # p "index"
         # p ar[index]
-      if ar[index2] == ar[index]
+      if ar[index_2] == ar[index]
         pairs += 1
         # ar.delete_at(index)
-        ar.delete_at(index2)
-        index2 == ar.length
+        ar.delete_at(index_2)
+        index_2 == ar.length
       end
     end
     # index += 1
     ar.delete_at(index)
-    index2 = index + 1
+    index_2 = index + 1
   end
   pairs
 end
 
-p sockMerchant([1, 2, 1, 2, 1, 3, 2])
-p sockMerchant([10, 20, 20, 10, 10, 30, 50, 10, 20])
+p sock_merchant([1, 2, 1, 2, 1, 3, 2])
+p sock_merchant([10, 20, 20, 10, 10, 30, 50, 10, 20])
