e850b759b6075f52dfd4dfd60f4f5a2134e1b1d4
seldenleonard <selden222@gmail.com>
2022-09-28 01:44:06 -0400

    adds link to problem on HackerRank

--- ruby-problems/sequence-equation-problem.rb
+++ ruby-problems/sequence-equation-problem.rb
@@ -1,9 +1,11 @@
+# https://www.hackerrank.com/challenges/permutation-equation/problem
+
 # INSTRUCTIONS
 # Given a sequence of n integers, p(1), p(2),..., p(n) where each element is distinct and satisfies 1 <= p(x) <= n. For each x where 1<= x <= n, that is x increments from 1 to n, find any integer y such that p(p(y)) = xx and keep a history of the values of y in a return array.
 
 # STEPS
 # 
 
-def permutation_equation(p)
+def sequence_equation(p)
   
 end
