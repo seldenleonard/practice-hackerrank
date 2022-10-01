bb3c954a9ed5f92d8a1e983f5613c2860b442490
seldenleonard <selden222@gmail.com>
2022-10-01 14:37:49 -0400

    adds instructions to between two sets algorithm

--- ruby-problems/between-two-sets-problem.rb
+++ ruby-problems/between-two-sets-problem.rb
@@ -1,5 +1,11 @@
 # https://www.hackerrank.com/challenges/between-two-sets/problem
 
+# INSTRUCTIONS
+# There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
+# The elements of the first array are all factors of the integer being considered
+# The integer being considered is a factor of all elements of the second array
+# These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
+
 # STEPS
 # Create a method that takes in two arrays
 # Set an index equal to zero for the first array
