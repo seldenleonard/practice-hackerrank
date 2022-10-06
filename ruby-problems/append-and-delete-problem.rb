5ede9933b8d3d39d99b90ac418fc44c10384f058
seldenleonard <selden222@gmail.com>
2022-10-06 02:56:59 -0400

    writes out whiteboarding steps for append and delete problem

--- ruby-problems/append-and-delete-problem.rb
+++ ruby-problems/append-and-delete-problem.rb
@@ -10,3 +10,16 @@
 # s = [a, b, c]
 # t = [d, e, f]
 # k = 6
+
+# STEPS
+# Define a method that accepts two arrays and an integer
+# If k is greater than or equal to s.length + t.length, return Yes
+# If k is less than s.length + t.length
+  # Define a variable called ops that will be used as the calculated minimum number of operations neccessary to convert s to t. Set it equal to s.length + t.length
+  # Define an index variable
+  # Using a while loop, iterate through s and t using the same index, comparing s[i] to t[i] until s[i] != t[i]
+    # For each instance where s[i] == t[i], reduce ops by 2
+  # If ops == k OR ops > k by an interval of 2 (so it could be greater by 2, 4, 6, 8, 10, etc.)
+    # return Yes
+  # Else
+    # return NO
