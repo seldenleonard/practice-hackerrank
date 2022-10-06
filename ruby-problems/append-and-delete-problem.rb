03fe2c88a0aa0b7ee4d157bf56918ea9a6cd9ae3
seldenleonard <selden222@gmail.com>
2022-10-06 03:15:21 -0400

    creates solution to append and delete algorithm that solves first test case

--- ruby-problems/append-and-delete-problem.rb
+++ ruby-problems/append-and-delete-problem.rb
@@ -12,14 +12,35 @@
 # k = 6
 
 # STEPS
-# Define a method that accepts two arrays and an integer
-# If k is greater than or equal to s.length + t.length, return Yes
+# Define a method that accepts two strings and an integer
+# If k is greater than or equal to s.length + t.length, return "Yes"
 # If k is less than s.length + t.length
-  # Define a variable called ops that will be used as the calculated minimum number of operations neccessary to convert s to t. Set it equal to s.length + t.length
-  # Define an index variable
+  # Define a variable called ops that will be used as the calculated minimum number of operations neccessary to convert s to t. Set it equal to s.length + t.length     *** consider just defining this earlier and replacing all instances of "s.length + t.length" with ops ***
+  # Define an index variable, set it equal to zero
   # Using a while loop, iterate through s and t using the same index, comparing s[i] to t[i] until s[i] != t[i]
     # For each instance where s[i] == t[i], reduce ops by 2
   # If ops == k OR ops > k by an interval of 2 (so it could be greater by 2, 4, 6, 8, 10, etc.)
-    # return Yes
+    # return "Yes"
   # Else
-    # return NO
+    # return "NO"
+
+def append_and_delete(s, t, k)
+  if k >= s.length + t.length
+    return "Yes"
+  end
+  if k < s.length + t.length
+    ops = s.length + t.length
+    i = 0
+    until s[i] != t[i]
+      ops -= 2
+      index += 1
+    end
+    if ops == k || ops > k && (ops % k) / 2 == 0
+      return "Yes"
+    else
+      return "No"
+    end
+  end
+end
+
+p append_and_delete("abc", "def", 6)
