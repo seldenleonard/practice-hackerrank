156819b69af9139e4448ff3188f9be602f74caaa
seldenleonard <selden222@gmail.com>
2022-11-23 11:39:01 -0500

    writes out whiteboarding steps to solve sequence equation

--- ruby-problems/sequence-equation-problem.rb
+++ ruby-problems/sequence-equation-problem.rb
@@ -1,10 +1,10 @@
 # https://www.hackerrank.com/challenges/permutation-equation/problem
 
 # INSTRUCTIONS
-# Given a sequence of n integers, p(1), p(2),..., p(n) where each element is distinct and satisfies 1 <= p(x) <= n. For each x where 1<= x <= n, that is x increments from 1 to n, find any integer y such that p(p(y)) = xx and keep a history of the values of y in a return array.
+# Given a sequence of n integers, p(1), p(2),..., p(n) where each element is distinct and satisfies 1 <= p(x) <= n. For each x where 1 <= x <= n, that is x increments from 1 to n, find any integer y such that p(p(y)) = xx and keep a history of the values of y in a return array.
 
 # STEPS
-# 
+# Given a sequence of positive, distinct integers do the following: using the values in the input array, and in the act of counting from 1 to the max value in ascending order, take each index and place it in a new array, with 1 (not 0) as the base index. Then return the array of indexes.
 
 def sequence_equation(p)
   
