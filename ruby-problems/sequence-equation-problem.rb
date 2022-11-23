7f0b2d96b656030b2fcc5e9a0f00cb0d005a5df7
seldenleonard <selden222@gmail.com>
2022-11-23 12:02:28 -0500

    fixes flawed whiteboarding steps and writes out method accordingly

--- ruby-problems/sequence-equation-problem.rb
+++ ruby-problems/sequence-equation-problem.rb
@@ -7,16 +7,28 @@
 # Clarified Instructions: Given a sequence of positive, distinct integers do the following: using the values in the input array, and in the act of counting from 1 to the max value in ascending order, take each index value and append it to a new array, with 1 (not 0) as the base index. Then return the array of indexes.
 # Start by defining a method that accepts an array
 # Define an index and set it equal to zero
-# Define an empty array
+# Define an empty array called 'indices'
 # Define a variable called 'counter' and set it equal to 1
-# Loop through the input array until counter variable is greater than to array.max
-  # Using a nest loop, loop through the array using the index until array[index] is equal to the counter variable
-  # Then append the empty array with the index value + 1
+# Loop through the input array until counter variable is greater than array.max
+  # Using a nested loop, loop through the array using the index until array[index] is equal to the counter variable
+    # Increase index by 1
+  # Then append the indices array with the index value + 1
   # Increase the counter variable by 1
   # Reset index to 0
 
 def sequence_equation(p)
-  
+  i = 0
+  indices = []
+  counter = 1
+  while counter <= p.max
+    until p[i] == counter
+      i += 1
+    end
+    indices << i + 1
+    counter += 1
+    i = 0
+  end
+  indices
 end
 
 p sequence_equation([2, 3, 1])
