023e63a6ded53bfd16b6ab3d4dfd5b929595a0dc
seldenleonard <selden222@gmail.com>
2022-12-11 22:33:12 -0500

    adds instructions from HackerRank site

--- ruby-problems/magic-square-forming-problem.rb
+++ ruby-problems/magic-square-forming-problem.rb
@@ -1,1 +1,5 @@
 # https://www.hackerrank.com/challenges/magic-square-forming/problem?isFullScreen=true
+
+# INSTRUCTIONS
+# We define a magic square to be an n * n matrix of distinct positive integers from 1 to n^2 where the sum of any row, column, or diagonal of length n is always equal to the same number: the magic constant.
+# You will be given a 3 * 3 matrix s of integers in the inclusive range [1, 9]. We can convert any digit a to any other digit b in the range [1, 9] at cost of |a- b|. Given s, convert it into a magic square at minimal cost. Print this cost on a new line.
