9328d2fcf13b63950c968aa15717b4bf2996a54a
seldenleonard <selden222@gmail.com>
2022-12-14 23:32:31 -0500

    starts writing whiteboarding steps - work in progress

--- ruby-problems/magic-square-forming-problem.rb
+++ ruby-problems/magic-square-forming-problem.rb
@@ -7,4 +7,6 @@
 # My Articulation of the Prompt
 # To make more understandable: Essentially, I need to change numbers in the matrix to make it into a magic square, but for every value I change (say I need to change a 7 to a 3), I have to do 7 - 3, which equals 4, and then i take that 4 and that is my cost. Then when I change another value, i add that to the cost. The goal is to make the neccessary changes to have a magic square and do so for the lowest possible cost. Another note, all subtractions when changing values are absolute value, so if I need to change a 4 to a 6, I do 4 - 6, which equals -2, but because its asolute value, the -2 becomes +2, and my cost is therefore +2.
 
+# STEPS
+# Define a method that accepts three arrays
 # 
