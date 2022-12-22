c9e847c539c9af7e706b9e7b21adf9b9e25a1536
seldenleonard <selden222@gmail.com>
2022-12-21 23:58:03 -0500

    writes out whiteboarding steps

---
+++ utopian-tree-problem.rb
@@ -0,0 +1,7 @@
+# STEPS
+# Create a function that accepts the array "n"
+# Set index to zero
+# Set a variable for height, "h", equal to 1
+# Create a while loop for when index is less than the length of array "n" 
+  # Within the while loop, create an if/else statement where "h" gets doubled if the n[index] value is odd, or (else) h increases by 1 if n[index] is positive
+# Return "h" for each value in array "n" (so "h" must be reset for each iteration of the while loop)
