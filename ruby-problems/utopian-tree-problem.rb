e94adbeb745ca31b8780feaac2b7e5363aa200b8
seldenleonard <selden222@gmail.com>
2022-12-21 23:59:26 -0500

    work in progress

--- ruby-problems/utopian-tree-problem.rb
+++ ruby-problems/utopian-tree-problem.rb
@@ -7,3 +7,7 @@
 # Create a while loop for when index is less than the length of array "n" 
   # Within the while loop, create an if/else statement where "h" gets doubled if the n[index] value is odd, or (else) h increases by 1 if n[index] is positive
 # Return "h" for each value in array "n" (so "h" must be reset for each iteration of the while loop)
+
+def utopian_tree()
+  
+end
