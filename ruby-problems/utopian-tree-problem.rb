f1030343fda4ee4ae48b8b1647f9181a2e944056
seldenleonard <selden222@gmail.com>
2023-01-16 23:43:33 -0500

    adds missing variable in instructions

--- ruby-problems/utopian-tree-problem.rb
+++ ruby-problems/utopian-tree-problem.rb
@@ -2,7 +2,7 @@
 
 # INSTRUCTIONS
 # The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer, its height increases by 1 meter.
-# A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring. How tall will the tree be after  growth cycles?
+# A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring. How tall will the tree be after n growth cycles?
 # For example, if the number of growth cycles is n = 5, the calculations are as follows:
 # Period  Height
   # 0       1
