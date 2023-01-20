dc86fdb8198fae3973ad4361ce99dc0abe3c83d6
seldenleonard <selden222@gmail.com>
2023-01-20 10:27:24 -0500

    Adds link to jumping clouds problem on hackerrank

--- ruby-problems/jumping-on-clouds-problem.rb
+++ ruby-problems/jumping-on-clouds-problem.rb
@@ -1,3 +1,4 @@
+# https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem?isFullScreen=true
 
 # INSTRUCTIONS
 # A child is playing a cloud hopping game. In this game, there are sequentially numbered clouds that can be thunderheads or cumulus clouds. The character must jump from cloud to cloud until it reaches the start again.
