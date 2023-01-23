3f3296fd8441fb97cc3c323963d3e8e753b6f0d3
seldenleonard <selden222@gmail.com>
2023-01-22 23:46:50 -0500

    work in progress

--- ruby-problems/jumping-on-clouds-problem.rb
+++ ruby-problems/jumping-on-clouds-problem.rb
@@ -5,3 +5,5 @@
 # There is an array of clouds, c and an energy level e = 100. The character starts from c[0] and uses 1 unit of energy to make a jump of size k to cloud c[(i + k) % n]. If it lands on a thundercloud, c[i] = 1, its energy (e) decreases by 2 additional units. The game ends when the character lands back on cloud 0.
 # Given the values of n, k, and the configuration of the clouds as an array c, determine the final value of e after the game ends.
 
+# STEPS
+# 
