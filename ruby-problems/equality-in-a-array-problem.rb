37db5306fb1e17a18a134b52cd9e1e5b6dfd61f3
seldenleonard <selden222@gmail.com>
2023-03-01 02:58:47 -0500

    writes out whiteboarding steps to solve equality-in-a-array-problem.rb

--- ruby-problems/equality-in-a-array-problem.rb
+++ ruby-problems/equality-in-a-array-problem.rb
@@ -13,3 +13,22 @@
 
 # Returns
   # int: the minimum number of deletions required
+
+# STEPS
+# Define a method that accepts an array
+# Define a variable called sorted_arr, set it equal to arr.sort
+# Define a variable called current_value, set it equal to sorted_arr[0]
+# Define a variable called current_count, set it equal to 1
+# Define a variable called max_value, set it equal to sorted_arr[0]
+# Define a variable called max_count, set it equal to 1
+# Define an index variable i, set it equal to 1
+# Loop through the values in sorted_arr starting at sorted_arr[i]
+  # If sorted_arr[1] == current_value
+    # current_count += 1
+  # Else
+    # If current_count > max_count
+      # max_value = current_value
+      # max_count = current_count
+      # current_value = sorted_arr[i]
+      # current_count = 0
+# Return sorted_arr.length - max_count
